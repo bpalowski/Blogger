ebb3218ba9d5e939f73234f50d93a12c5c15288c
Brian Palowski <brianpalowski@Brians-MacBook-Pro.local>
2020-08-31 22:24:55 -0700

    log out done

--- client/src/state/middleware/auth.js
+++
@@ -1,3 +0,0 @@
-// import axios from "axios";
-// import { API } from "../actions/types";
-// import { accessDenied, apiError, apiStart, apiEnd } from "../actions/api";
--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
 
-module.exports = function (passport) {
+module.exports = (passport) => {
   passport.use(
     new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
