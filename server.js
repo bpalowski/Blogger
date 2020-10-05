0ef2c25362ddf1b4ee574c0d031e2bcd90a73919
Brian Palowski <brianpalowski@Brians-MacBook-Pro.local>
2020-10-04 20:39:36 -0700

    bugfix, in a

--- server.js
+++ server.js
@@ -64,5 +64,13 @@ app.use('/blog', blog)
 // server
 const PORT = process.env.PORT || 5000
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 //Sockit
 setServerIO(app.listen(PORT, console.log(`Listening to ${PORT} on ${process.env.NODE_ENV} enviroment `)))
