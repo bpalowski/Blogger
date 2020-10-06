2b7a07baa626285ab0c91eec38afd702e3202c59
Brian Palowski <brianpalowski@Brians-MacBook-Pro.local>
2020-10-06 11:54:32 -0700

    updated build

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = (passport) => {
     new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: 'http://blogggers.herokuapp.com/auth/google/callback',
     },
       async (accessToke, refreshToken, profile, callback) => {
         const admins = await Admin.findOne({ email: profile.emails[0].value }).exec() ? true : false
