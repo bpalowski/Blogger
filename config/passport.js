bf023909824cc9e5be56c46b990f504c616968a7
Brian Palowski <brianpalowski@Brians-MacBook-Pro.local>
2020-10-06 16:29:11 -0700

    set up heroku script 7

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ module.exports = (passport) => {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-      callbackURL: '/auth/google/callback',
+      callbackURL: 'http://blogggers.herokuapp.com/auth/google/callback',
 
     },
       async (accessToke, refreshToken, profile, callback) => {
